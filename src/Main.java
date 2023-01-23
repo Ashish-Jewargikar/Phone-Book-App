4b865d0eed1363940f26ffa29549314a59cbb298
AshIsh <ashish961169@gmail.com>
2023-01-23 19:44:02 +0530

    phone 2

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
         LandLine l1 = new LandLine("123");
         LandLine l2 = new LandLine("345");       
-        l1.callNumber("345");
+        l1.callNumber("123");
         l2.receiveCall("345");
         System.out.println(l2.answerCall());
 	}
